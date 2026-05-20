54583703c837c2c237e8c385ecace227cfdaa99c
Joris Guffens <joris.guffens@outlook.com>
2026-05-20 14:25:59 +0200

    add support for config with toml serializable

--- api/src/main/java/com/gufli/brick/orm/api/database/DatabaseConfig.java
+++ api/src/main/java/com/gufli/brick/orm/api/database/DatabaseConfig.java
@@ -1,12 +1,12 @@
 package com.gufli.brick.orm.api.database;
 
-public class DatabaseConfig {
+import io.github.wasabithumb.jtoml.serial.TomlSerializable;
+
+public class DatabaseConfig implements TomlSerializable {
 
     public String dsn;
     public String driver;
     public String username;
     public String password;
-
-    public boolean debug;
 
 }
